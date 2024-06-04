df8a585f4448f46b9eee08ca97374dd0a9e12691
1nhann <inhannsec@gmail.com>
2024-06-04 13:50:30 +0800

    不显示静态资源的url

--- js/popup.js
+++ js/popup.js
@@ -126,14 +126,33 @@ function httpGet(url, callback){
 
 function extract_URL(htmlDatas) {
     // 使用正则表达式匹配 URL
-    const matches = htmlDatas.match(/(?:"|')(((?:[a-zA-Z]{1,10}:\/\/|\/\/)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']{0,})|((?:\/|\.\.\/|\.\/)[^"'><,;| *()(%%$^\/\\\[\]][^"'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}\/[a-zA-Z0-9_\-/]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|\/][^"|']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:php|asp|aspx|jsp|json|action|html|js|txt|xml)(?:\?[^"|']{0,}|)))(?:"|')/g);
+    const urlPattern = /(?:"|')(((?:[a-zA-Z]{1,10}:\/\/|\/\/)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']*)|((?:\/|\.\.\/|\.\/)[^"'><,;| *()(%%$^\/\\\[\]][^"'><,;|()]*)|([a-zA-Z0-9_\-/]+\/[a-zA-Z0-9_\-/]+\.(?:[a-zA-Z]{1,4}|action)(?:[\?|\/][^"|']*)?)|([a-zA-Z0-9_\-]+\.(?:php|asp|aspx|jsp|json|action|html|js|txt|xml)(?:\?[^"|']*)?))(?:"|')/g;
 
+    // 黑名单后缀列表
+    const blacklist = [
+        ".html", ".webm", ".doc", ".docx", ".zip", ".rar", ".pdf", ".png", 
+        ".jpeg", ".jpg", ".css", ".js", ".gif", ".svg", ".woff", ".mp4", 
+        ".mp3", ".woff2", ".woff", ".ico", ".ttf", ".otf", ".min.map", 
+        ".css.map", ".js.map"
+    ];
+
+    // 获取所有匹配的 URL
+    const matches = htmlDatas.match(urlPattern);
+
+    // 如果没有匹配项，返回空数组
     if (!matches) {
         return [];
     }
 
-    // 去掉引号
-    return matches.map(match => match.replace(/['"]/g, ''));
+    // 去掉每个匹配项的开头和结尾的引号
+    const urls = matches.map(match => match.slice(1, -1));
+
+    // 过滤掉包含黑名单后缀的 URL
+    const filteredUrls = urls.filter(url => 
+        !blacklist.some(ext => url.toLowerCase().endsWith(ext))
+    );
+
+    return filteredUrls;
 }
 
 function unique(arr){
