b85b25a4e825e4acc41394c118fb80f5e682a90e
1nhann <inhannsec@gmail.com>
2024-06-04 12:52:58 +0800

    去掉引号

--- js/popup.js
+++ js/popup.js
@@ -124,9 +124,16 @@ function httpGet(url, callback){
 	request.send(null);
 }
 
-function extract_URL(htmlDatas){
-	return htmlDatas.match(/(?:"|')(((?:[a-zA-Z]{1,10}:\/\/|\/\/)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']{0,})|((?:\/|\.\.\/|\.\/)[^"'><,;| *()(%%$^\/\\\[\]][^"'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}\/[a-zA-Z0-9_\-/]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|\/][^"|']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:php|asp|aspx|jsp|json|action|html|js|txt|xml)(?:\?[^"|']{0,}|)))(?:"|')/g);
+function extract_URL(htmlDatas) {
+    // 使用正则表达式匹配 URL
+    const matches = htmlDatas.match(/(?:"|')(((?:[a-zA-Z]{1,10}:\/\/|\/\/)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']{0,})|((?:\/|\.\.\/|\.\/)[^"'><,;| *()(%%$^\/\\\[\]][^"'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}\/[a-zA-Z0-9_\-/]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|\/][^"|']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:php|asp|aspx|jsp|json|action|html|js|txt|xml)(?:\?[^"|']{0,}|)))(?:"|')/g);
 
+    if (!matches) {
+        return [];
+    }
+
+    // 去掉引号
+    return matches.map(match => match.replace(/['"]/g, ''));
 }
 
 function unique(arr){
